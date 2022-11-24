4abb0a2f0bba3f0615f2f8250b0688d87262a899
otaviopato <otaviopato2017@outlook.com>
2022-11-24 04:18:17 -0300

    feat: model of Student

---
+++ martial.arts/src/main/java/com/br/martial/arts/martial/arts/model/Student.java
@@ -0,0 +1,34 @@
+package com.br.martial.arts.martial.arts.model;
+
+import java.sql.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
+public class Student {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+  private String name;
+  private String email;
+  private String cpf;
+  private String phone;
+  private String address;
+  @JsonFormat(pattern = "dd-MM-yyyy")
+  private Date birthDate;
+}
